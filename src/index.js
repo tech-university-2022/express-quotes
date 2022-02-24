deef8a3fd9c4af5b41251cc51d20b5f470a15157
Nandika <Nandika_Arora@mckinsey.com>
2022-02-24 23:01:07 +0530

    feat: add get quotes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ const express = require('express');
 const env = require('dotenv');
 const { healthRouter } = require('./routes/health.route');
 const { quotesRouter } = require('./routes/quotes.route');
-// const { specifiedQuoteRouter } = require('./routes/specifiedQuote.route');
 
 env.config();
 
@@ -12,10 +11,6 @@ const app = express();
 
 app.use('/health', healthRouter);
 app.use('/get-quotes', quotesRouter);
-// app.use('/get-quotes', specifiedQuoteRouter);
-// app.get('/health', (req, res) => {
-//   res.end('hey');
-// });
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
