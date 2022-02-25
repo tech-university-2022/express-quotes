e55b3377a739d856c9eecf9e41c637747999acab
vrisatija <99251026+vrisatija@users.noreply.github.com>
2022-02-25 11:28:18 +0530

    feat: fix quotes service

--- src/services/quotes.services.js
+++ src/services/quotes.services.js
@@ -1,11 +1,11 @@
 const axios = require('axios').default;
 
-const firstFiftyQuotes = async (req, res) => {
+const firstFiftyQuotes = async () => {
   const quotes = await axios.get('https://type.fit/api/quotes');
   return quotes.data.slice(0, 50);
 };
 
-const QuoteAtIndex = async (req, res) => {
+const QuoteAtIndex = async (req) => {
   const quotes = await axios.get('https://type.fit/api/quotes');
   return quotes.data[req.params.index];
 };
